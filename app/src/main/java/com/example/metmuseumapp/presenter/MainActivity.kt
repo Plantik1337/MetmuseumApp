8491b73f20e8287b275d275b5a23b4eaf3e10b85
Ashipilov <Thesol590@yandex.ru>
2025-01-16 13:40:14 +0300

    Ui

--- app/src/main/java/com/example/metmuseumapp/presenter/MainActivity.kt
+++ app/src/main/java/com/example/metmuseumapp/presenter/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.metmuseumapp.presenter
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isGone
 import com.bumptech.glide.Glide
+import com.example.metmuseumapp.ObjectInfo
 import com.example.metmuseumapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -21,22 +23,68 @@ class MainActivity : AppCompatActivity() {
 
 
         viewModel.currentObjectLiveData.observe(this) { objectInfo ->
-            Glide.with(binding.imageView)
-                .load(objectInfo.primaryImage)
-                .into(binding.imageView)
-
-            binding.titleTV.text = objectInfo.title
-            binding.dateTV.text = "${objectInfo.objectBeginDate} - ${objectInfo.objectEndDate}"
-
-            binding.artistTV.text = "${objectInfo.artistDisplayName}, ${objectInfo.artistRole}"
-
-            binding.departmentTV.text = "${objectInfo.department}, ${objectInfo.culture}"
-
-            binding.mediumTV.text = "${objectInfo.medium}"
-
-            binding.dimensionsTV.text = "${objectInfo.dimensions}"
-
+            dataInteractor(objectInfo)
         }
 
+    }
+
+    fun dataInteractor(objectInfo: ObjectInfo) {
+        binding.progressBar.isGone = true
+
+        Glide.with(binding.imageView).load(objectInfo.primaryImage).into(binding.imageView)
+
+        binding.titleTV.isGone = false
+        binding.dateTV.isGone = false
+
+        binding.titleTV.text = objectInfo.title
+        binding.dateTV.text = "${objectInfo.objectBeginDate} - ${objectInfo.objectEndDate}"
+
+        if (objectInfo.artistRole.isNotEmpty()) {
+            binding.artistRole.isGone = false
+            binding.artistRoleTV.text = objectInfo.artistRole
+            binding.artistRoleTV.isGone = false
+        }
+
+        if (objectInfo.artistDisplayName.isNotEmpty()) {
+            binding.artistTitle.isGone = false
+            binding.artistTV.text = objectInfo.artistDisplayName
+            binding.artistTV.isGone = false
+        }
+
+        if (objectInfo.artistDisplayBio.isNotEmpty()) {
+            binding.artistBio.isGone = false
+            binding.artistBioTV.text = objectInfo.artistDisplayBio
+            binding.artistBioTV.isGone = false
+        }
+
+        if (objectInfo.department.isNotEmpty()) {
+            binding.departmentTitle.isGone = false
+            binding.departmentTV.text = objectInfo.department
+            binding.departmentTV.isGone = false
+        }
+
+        if (objectInfo.culture.isNotEmpty()) {
+            binding.cultureTitle.isGone = false
+            binding.cultureTV.text = objectInfo.culture
+            binding.cultureTV.isGone = false
+        }
+
+        if (objectInfo.period.isNotEmpty()) {
+            binding.periodTitle.isGone = false
+            binding.periodTV.text = objectInfo.period
+            binding.periodTV.isGone = false
+        }
+
+        if (objectInfo.medium.isNotEmpty()) {
+            binding.mediumTitle.isGone = false
+            binding.mediumTV.text = objectInfo.medium
+            binding.mediumTV.isGone = false
+        }
+
+        if (objectInfo.dimensions.isNotEmpty()) {
+            binding.dimensionsTitle.isGone = false
+            binding.dimensionsTV.text = objectInfo.dimensions
+            binding.dimensionsTV.isGone = false
+        }
     }
 }
